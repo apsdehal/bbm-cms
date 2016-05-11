0c23d01fb799aaa12e761b119df283cd3128852e
Amanpreet Singh <apsdehal@gmail.com>
2016-05-11 11:28:53 +0530

    Create basic content controller

--- web/js/controllers/ContentController.js
+++ web/js/controllers/ContentController.js
@@ -1,11 +1,15 @@
 
-function ContentController($rootScope, $scope) {
+function ContentController($rootScope, $scope, $state) {
     // set sidebar closed and body solid layout mode
     $rootScope.settings.layout.pageContentWhite = true;
     $rootScope.settings.layout.pageBodySolid = true;
     $rootScope.settings.layout.pageSidebarClosed = true;
+
+    $scope.isActive = function (name) {
+      return $state.current.name === ('content.' + name);
+    }
 };
 
-ContentController.$inject = ['$rootScope', '$scope'];
+ContentController.$inject = ['$rootScope', '$scope', '$state'];
 
 bbmCms.controller('ContentController', ContentController);
