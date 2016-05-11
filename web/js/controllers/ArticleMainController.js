2331fe3acafbe43de4b430f02bd9f498c0c98c3a
Amanpreet Singh <apsdehal@gmail.com>
2016-05-11 11:28:29 +0530

    Working article page

--- web/js/controllers/ArticleMainController.js
+++ web/js/controllers/ArticleMainController.js
@@ -1,16 +1,51 @@
-
 function ArticleMainController($rootScope, $scope, Article) {
-    Article.find(
-      {filter:
-        {order: 'created DESC',
-         limit: 3}},
-      function (list) {
-        console.log(list);
-      },
-      function (list) {
-        console.log(list);
-      }
-    );
+  $scope.articles = [];
+  $scope.currentArticle = false;
+  $scope.isEditActive = true;
+
+  $scope.editButtonText = 'render';
+  $scope.toggleEdit = function () {
+    $scope.isEditActive = !$scope.isEditActive;
+
+    if ($scope.isEditActive) {
+      $scope.editButtonText = 'render';
+    } else {
+      $scope.editButtonText = 'edit';
+    }
+  }
+  var getCurrentArticleTags = function () {
+    if (!$scope.currentArticle) {
+      return '';
+    }
+
+    var x = [];
+    x.push($scope.currentArticle.tags.map(function (tag) {
+      return tag.name;
+    }));
+
+    return x.join(',');
+  };
+
+  $scope.changeCurrentArticle = function (index) {
+    if ($scope.articles.length > index) {
+      $scope.currentArticle = $scope.articles[index];
+    }
+  }
+
+  $scope.$watch('currentArticle', function (newValue, oldValue) {
+    if (newValue !== oldValue) {
+      $scope.currentArticleTags = getCurrentArticleTags();
+    }
+  })
+  Article.find(
+    {filter:
+      {order: 'created DESC',
+       limit: 3}},
+    function (list) {
+      $scope.articles = list;
+      $scope.currentArticle = list[0];
+    }
+  );
 };
 
 ArticleMainController.$inject = ['$rootScope', '$scope', 'Article'];
