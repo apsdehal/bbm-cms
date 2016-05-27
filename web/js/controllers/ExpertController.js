0ee2b306ea48c73c2551105be19f5e0ec46f32a7
Amanpreet Singh <apsdehal@gmail.com>
2016-05-27 14:54:58 +0530

    Load experts from search api and show them paginated

--- web/js/controllers/ExpertController.js
+++ web/js/controllers/ExpertController.js
@@ -2,6 +2,9 @@ function ExpertController($scope, ExpertService) {
 
   $scope.currentExpert = {};
   $scope.experts = [];
+  $scope.totalExperts = 0;
+  $scope.itemsPerPage = 10;
+  $scope.maxSize = 5;
 
   $scope.getExperts = function (val) {
     return ExpertService.search(val).then(function (data) {
@@ -9,6 +12,14 @@ function ExpertController($scope, ExpertService) {
       return data.response.numFound ? data.response.docs : [];
     });
   }
+
+
+  ExpertService.getExperts(1).then(function (data) {
+    data = data.data;
+    $scope.totalExperts = data.response.numFound;
+    $scope.experts = data.response.docs;
+    $scope.numPages = $scope.totalExperts / $scope.itemsPerPage;
+  });
 }
 
 ExpertController.$inject = ['$scope', 'ExpertService'];
